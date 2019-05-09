4a6a3513b3544b297fb74fc0ac8f2e5440df7c7a
hpnightowl <iamhp2k@gmail.com>
2019-05-09 06:48:26 +0530

    lettuce: Rename Device Bluetooth Name
    Signed-off-by: hpnightowl <iamhp2k@gmail.com>

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME        "YU5010"
+#define BTM_DEF_LOCAL_NAME        "Yuphoria"
 
 #define BLUETOOTH_QTI_SW          TRUE
 #define BT_CLEAN_TURN_ON_DISABLED TRUE
